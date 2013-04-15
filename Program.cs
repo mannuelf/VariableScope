8e40e5034c26b15332a8f68f194dc2c147f53b2c
Mannuel <mannuel.ferreira@gmail.com>
2013-04-15 15:41:17 +0200

    Variable scope
    C# Variable scope

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableScope
+{
+    class Program
+    {
+        // fields to contain the class data 
+        static void Main(string[] args)
+        {
+            int i = 800000;
+            short x = 5;
+            float f = 20.0f;
+
+            f = i;
+
+            i = (int)f;
+            Console.ReadLine();
+        }
+    }
+}
